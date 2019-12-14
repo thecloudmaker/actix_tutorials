75fb16b358b35a6ac54fb4368871bd4e242faa7c
Tore Skog Pettesen <toreskog@live.com>
2019-12-14 12:41:00 +0100

    Authenticaiton tutorial

---
+++ authentication/src/auth/routes.rs
@@ -0,0 +1,70 @@
+use crate::api_error::ApiError;
+use crate::user::{User, UserMessage};
+use actix_web::{post, get, web, HttpResponse};
+use actix_session::Session;
+use serde_json::json;
+use uuid::Uuid;
+
+#[post("/register")]
+fn register(user: web::Json<UserMessage>) -> Result<HttpResponse, ApiError> {
+    let user = User::create(user.into_inner())?;
+    Ok(HttpResponse::Ok().json(user))
+}
+
+#[post("/sign-in")]
+fn sign_in(credentials: web::Json<UserMessage>, session: Session) -> Result<HttpResponse, ApiError> {
+    let credentials = credentials.into_inner();
+
+    let user = User::find_by_email(credentials.email)
+        .map_err(|e| {
+            match e.status_code {
+                404 => ApiError::new(401, "Credentials not valid!".to_string()),
+                _ => e,
+            }
+        })?;
+    
+    let is_valid = user.verify_password(credentials.password.as_bytes())?;
+
+    if is_valid == true {
+        session.set("user_id", user.id)?;
+        session.renew();
+
+        Ok(HttpResponse::Ok().json(user))
+    }
+    else {
+        Err(ApiError::new(401, "Credentials not valid!".to_string()))
+    }
+}
+
+#[post("/sign-out")]
+fn sign_out(session: Session) -> Result<HttpResponse, ApiError> {
+    let id: Option<Uuid> = session.get("user_id")?;
+
+    if let Some(_) = id {
+        session.purge();
+        Ok(HttpResponse::Ok().json(json!({ "message": "Successfully signed out" })))
+    }
+    else {
+        Err(ApiError::new(401, "Unauthorized".to_string()))
+    }
+}
+
+#[get("/who-am-i")]
+fn who_am_i(session: Session) -> Result<HttpResponse, ApiError> {
+    let id: Option<Uuid> = session.get("user_id")?;
+
+    if let Some(id) = id {
+        let user = User::find(id)?;
+        Ok(HttpResponse::Ok().json(user))
+    }
+    else {
+        Err(ApiError::new(401, "Unauthorized".to_string()))
+    }
+}
+
+pub fn init_routes(cfg: &mut web::ServiceConfig) {
+    cfg.service(register);
+    cfg.service(sign_in);
+    cfg.service(sign_out);
+    cfg.service(who_am_i);
+}
---
+++ authentication/src/db.rs
@@ -0,0 +1,31 @@
+use crate::api_error::ApiError;
+use diesel::pg::PgConnection;
+use diesel::r2d2::ConnectionManager;
+use lazy_static::lazy_static;
+use r2d2;
+use std::env;
+
+type Pool = r2d2::Pool<ConnectionManager<PgConnection>>;
+pub type DbConnection = r2d2::PooledConnection<ConnectionManager<PgConnection>>;
+
+embed_migrations!();
+
+lazy_static! {
+    static ref POOL: Pool = {
+        let db_url = env::var("DATABASE_URL").expect("Database url not set");
+        let manager = ConnectionManager::<PgConnection>::new(db_url);
+        Pool::new(manager).expect("Failed to create db pool")
+    };
+}
+
+pub fn init() {
+    info!("Initializing DB");
+    lazy_static::initialize(&POOL);
+    let conn = connection().expect("Failed to get db connection");
+    embedded_migrations::run(&conn).unwrap();
+}
+
+pub fn connection() -> Result<DbConnection, ApiError> {
+    POOL.get()
+        .map_err(|e| ApiError::new(500, format!("Failed getting db connection: {}", e)))
+}
